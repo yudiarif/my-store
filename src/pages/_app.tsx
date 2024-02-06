892802b130e0423f57e99bcc74652ead9af61831
yudiarif <yudiarif@student.untan.ac.id>
2024-02-06 17:41:10 +0700

    feat: register

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={poppins.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
